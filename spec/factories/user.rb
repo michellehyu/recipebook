ae56dcebdb3a338d5c2711d557152216a036cc65
Chris Peak <christopeak@gmail.com>
2015-08-03 21:35:57 -0700

    regenerated factory, this time via rails generate

--- spec/factories/user.rb
+++
@@ -1,6 +0,0 @@
-FactoryGirl.define do
-  factory :user do
-    name 'Harry'
-    email 'harry@potter.com'
-  end
-end
---
+++ spec/factories/users.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :user do
+    name "MyString"
+    email 'harry@hogwarts.edu'
+  end
+
+end
