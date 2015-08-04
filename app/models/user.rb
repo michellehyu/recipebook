a76824ceba29fd3c9f9313956c39e927093f5c02
Chris Peak <christopeak@gmail.com>
2015-08-04 08:11:10 -0700

    add has_secure_password to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
+require 'securerandom'
+
 class User < ActiveRecord::Base
+  has_secure_password
+
+  validates_uniqueness_of :email
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,7 @@ FactoryGirl.define do
   factory :user do
     name "Harry"
     email 'harry@hogwarts.edu'
+    password "password"
   end
 
 end
