cd98d3081880266938cc7d89bc03ce56f3849876
Chris Peak <christopeak@gmail.com>
2015-08-06 11:30:17 -0700

    added specs

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,11 +2,15 @@ require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
 
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+  let(:user) { FactoryGirl.build(:user) }
+
+  describe "#create" do
+    it "adds a User session" do
+      post :create, email: user.email, password: user.password
+    end
+    it "redirects to welcome page" do
+      post :create, email: user.email, password: user.password
+      expect(response).to redirect_to(welcome_path)
     end
   end
-
 end
