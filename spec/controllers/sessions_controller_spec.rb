03025567577a7cb9a7754d93b695794ffe81538d
Chris Peak <christopeak@gmail.com>
2015-08-09 17:03:06 -0700

    added controller specs for sessions#destroy

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -5,12 +5,28 @@ RSpec.describe SessionsController, type: :controller do
   let(:user) { FactoryGirl.create(:user) }
 
   describe "#create" do
-    it "adds a User session" do
-      post :create, email: user.email, password: user.password
+    context 'when a user is not already signed in' do
+      it "adds a User session" do
+        post :create, email: user.email, password: user.password
+      end
+      it "redirects to welcome page" do
+        post :create, email: user.email, password: user.password
+        expect(response).to redirect_to(welcome_path)
+      end
     end
-    it "redirects to welcome page" do
-      post :create, email: user.email, password: user.password
-      expect(response).to redirect_to(welcome_path)
+  end
+
+  describe '#destroy' do
+    context 'when a user is already signed in' do
+
+      before do
+        post :create, email: user.email, password: user.password
+      end
+
+      it 'redirects to welcome page' do
+        get :destroy
+        expect(response).to redirect_to(welcome_path)
+      end
     end
   end
 end
--- spec/support/authentication.rb
+++
@@ -1,8 +0,0 @@
-include SessionsHelper
-
-def login_as(user)
-  visit login_path
-  fill_in 'Email', with: user.email 
-  fill_in 'Password', with: 'password'
-  click_button 'Login'
-end
