73fedd6d019576dec68e0e52e858b9afb5202575
Chris Peak <christopeak@gmail.com>
2015-08-28 08:10:53 -0700

    added and cleaned up some tests

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "noreply@recipebook.com"
   layout 'mailer'
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-  default from: "nooreply@recipebook.com"
+  default from: "noreply@recipebook.com"
 
   def welcome_email(user)
     @user = user
